88278ad87004d5464cc220c715f2739df7385bd3
sushmarajpeketi <sushmaraj808@gmail.com>
2023-11-02 23:01:24 +0530

    error handlers middlewares and function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,22 +1,33 @@
-import User from '../models/user.model.js'
+import User from "../models/user.model.js";
+import express from 'express'
+import {errorHandler} from '../utils/error.js'
+import bcryptjs from "bcryptjs";
 // import bcrypt from 'bcrypt'
-import bcryptjs from 'bcryptjs'
- function createResponse(ok,message,data){
-    return {ok,message,data}
-}
-export const signup = async (req,res)=>{
-    const {username,email,password} = req.body
-    // const hashedPassword = await bcrypt.hash(password,10)
-    const hashedPassword = bcryptjs.hashSync(password,10)
-    // console.log("hashedPassword password is "+ hashedPassword)
-    const newUser = new User({username,email,password:hashedPassword})
-    try{
-        await newUser.save()
-        res.status(201).json(createResponse(true,"user is created succesffully",{username,email}))
-    }
-    catch(err){
-        res.status(500).json(createResponse(false,"error during creating user",err.message ))
-    }
-    
 
+
+function createResponse(ok, message, data) {
+  return { ok, message, data };
 }
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  // const hashedPassword = await bcrypt.hash(password,10)
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  // console.log("hashedPassword password is "+ hashedPassword)
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res
+      .status(201)
+      .json(
+        createResponse(true, "user is created succesffully", {
+          username,
+          email,
+        })
+      );
+  } catch (err) {
+    next(err);
+    // res.status(500).json(createResponse(false,"error during creating user",err.message ))
+  }
+};
+
+
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+
 dotenv.config();
 
 const app = express();
@@ -21,3 +22,13 @@ mongoose
 
   app.use('/api/user',userRouter)
   app.use('/api/auth',authRouter)
+
+  app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal server error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+  })
