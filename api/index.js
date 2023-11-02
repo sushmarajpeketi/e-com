a7eed414e64596a7ea20aecc4d00bd64cd381fc7
sushmarajpeketi <sushmaraj808@gmail.com>
2023-11-02 19:00:39 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-mongoose
+mongoose      
   .connect(process.env.MONGO)
   .then(() => {
     console.log("connected to MongoDB");
