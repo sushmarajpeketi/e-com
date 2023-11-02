29aa7fe9df8e33979dd62695970088ad8762b77d
sushmarajpeketi <sushmaraj808@gmail.com>
2023-11-02 17:54:32 +0530

    frontEnd & backEnd setUp

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+
+
+const App = () => {
+  return (
+    
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/home' element={<Home/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
