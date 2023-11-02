b02cc04aabbd4b0e13c900584300b70509a324aa
sushmarajpeketi <sushmaraj808@gmail.com>
2023-11-02 19:20:16 +0530

    create test api route,controller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 const app = express();
@@ -15,3 +16,5 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  app.use('/api/user',userRouter)
