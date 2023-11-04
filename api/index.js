125c8b1fa6e9da509042b6a676a59db442c6c99a
sushmarajpeketi <sushmaraj808@gmail.com>
2023-11-04 21:31:57 +0530

    create user,update api,profile page creation

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 app.use(express.json())
+app.use(cookieParser())
+
 mongoose      
   .connect(process.env.MONGO)
   .then(() => {
@@ -22,6 +24,7 @@ mongoose
 
   app.use('/api/user',userRouter)
   app.use('/api/auth',authRouter)
+  
 
   app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500
